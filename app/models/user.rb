a4e4d58f1b51e8688c14ce31a2cebc980e711acc
Deborah Kwon <deborah.m.kwon@gmail.com>
2016-01-15 12:52:22 -0500

    added devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
-before_action :authenticate_user! #sets up a controller with user authentication, (assuming your devise model is 'User')
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def new
+  end
 end
